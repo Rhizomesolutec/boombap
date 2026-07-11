9cb69aabe8bdd92d5fd98b9d9bf06f7e04f3dc94
Adhil Ahmmed <adhilahammed7792@gmail.com>
2026-07-12 00:27:50 +0530

    SAB6 updates: update Instagram URL to use hl=en format

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -134,7 +134,7 @@ export default function Home() {
                   <span className="font-proxima text-[9px] font-bold uppercase tracking-[0.25em] text-white/30">Connect</span>
                   <div className="flex items-center gap-3">
                     {[
-                      { img: "/SAB6/Instagram.jpg", url: "https://www.instagram.com/lil_payyan?igsh=MW9kajd5YmFwMTRjaw==" },
+                      { img: "/SAB6/Instagram.jpg", url: "https://www.instagram.com/lil_payyan/?hl=en" },
                       { img: "/SAB6/Whatsapp.jpg", url: "https://wa.me/919567355168" },
                       { img: "/SAB6/Youtube.jpg", url: "https://youtube.com/channel/UCw_V8qjy-aEvNHoXzq7ysaA?si=eFaatKkJJOTeF5X0" }
                     ].map((soc, idx) => (
